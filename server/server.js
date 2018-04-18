f39b55705f84d6deae59632f3d48111bd150a97d
jsPasha <31847282+jsPasha@users.noreply.github.com>
2018-04-18 23:12:42 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mangoos');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mangoos');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -16,22 +17,41 @@ app.post('/todos', (req, res) => {
 
 	todo.save().then((doc) => {
 		res.send(doc);
-	}).catch((e)=>{
+	}).catch((e) => {
 		res.status(400).send(e);
 	});
 });
 
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
-		res.send({todos});
+		res.send({ todos });
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (ObjectID.isValid(id)) {
+		Todo.findById(id).then((todo) => {
+			if (todo) {
+				res.send(todo);
+			} else {
+				res.status(404).send();
+			}
+		}, e => {
+			res.status(400).send();
+		})
+	} else {
+		res.status(404).send();
+	}
+
 });
 
 app.listen(3000, () => {
 	console.log('Started on port 3000')
 });
 
-module.exports = {app}
+module.exports = { app }
 
