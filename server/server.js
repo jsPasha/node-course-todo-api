3a317220163a163b62a1965f6aaf0c625a71925e
DESKTOP-O0ACO2G\Nata <asd>
2018-04-19 12:29:26 +0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var { ObjectID } = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-var { mongoose } = require('./db/mangoos');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+const { mongoose } = require('./db/mangoos');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const port = process.env.PORT || 3000;
 
@@ -53,21 +54,42 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
 	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({ todo });
+	}, e => {
+		res.status(400).send();
+	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
 
 	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
-	Todo.findByIdAndRemove(id).then((todo) => {
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
 		if (!todo) {
-			return res.status(404).send();	
+			return res.status(400).send()
 		}
-		
-		res.send({ todo });
+		res.send({todo})
 	}, e => {
-		res.status(400).send();
+		res.status(400).send()
 	});
-	
 });
 
 app.listen(port, () => {
