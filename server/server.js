9c5902d509b68babab5d09334d658f26d4c763cb
DESKTOP-O0ACO2G\Nata <asd>
2018-04-19 10:56:23 +0300

    Add test cases for get todos by id

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get('/todos/:id', (req, res) => {
 	if (ObjectID.isValid(id)) {
 		Todo.findById(id).then((todo) => {
 			if (todo) {
-				res.send(todo);
+				res.send({todo});
 			} else {
 				res.status(404).send();
 			}
