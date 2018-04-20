00a73cfd88c4ebd70849df4113e6f9d2e7738b70
DESKTOP-O0ACO2G\Nata <asd>
2018-04-20 13:27:40 +0300

    not fix

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -31,19 +31,22 @@ const todos = [{
 }];
 
 const populateTodos = (done) => {
+
 	Todo.remove({}).then(() => {
 		return Todo.insertMany(todos);
-	}).then(() => done());
+	}).then(() => {
+
+		done()
+	});
 };
 
 const populateUsers = (done) => {
-	User.remove({}).then(() => {done()});
-	// User.remove({}).then(() => {
-	// 	var userOne = new User(users[0]).save();
-	// 	var userTwo = new User(users[1]).save();
-
-	// 	return Promise.all([userOne, userTwo])
-	// }).then(() => done());
+	User.remove({}).then(() => {
+		var userOne = new User(users[0]).save();
+		var userTwo = new User(users[1]).save();
+		
+		return Promise.all([userOne, userTwo])
+	}).then(done);
 };
 
 module.exports = { todos, populateTodos, users, populateUsers };
