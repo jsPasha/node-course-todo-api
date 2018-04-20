ab9f824ef706014d73a5a9f32e91986544d0f76b
DESKTOP-O0ACO2G\Nata <asd>
2018-04-20 12:34:49 +0300

    logout

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,48 +1,49 @@
 const { ObjectID } = require('mongodb');
-const { Todo } = require('./../../models/todo');
-const { User } = require('./../../models/user');
 const jwt = require('jsonwebtoken');
 
-const userOneId = new ObjectID;
-const userTwoId = new ObjectID;
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
 
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
 const users = [{
 	_id: userOneId,
-	email: 'test@exam.com',
-	password: 'userOnePath',
+	email: 'andrew@example.com',
+	password: 'userOnePass',
 	tokens: [{
 		access: 'auth',
-		token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+		token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString()
 	}]
 }, {
 	_id: userTwoId,
-	email: 'test2@exam.com',
-	password: 'userTwoPath',
-}]; 
+	email: 'jen@example.com',
+	password: 'userTwoPass'
+}];
 
 const todos = [{
-	_id: new ObjectID,
-	text: 'First text todo'
+	_id: new ObjectID(),
+	text: 'First test todo'
 }, {
-	_id: new ObjectID,
+	_id: new ObjectID(),
 	text: 'Second test todo',
 	completed: true,
-	completedAt: 123
+	completedAt: 555
 }];
 
 const populateTodos = (done) => {
 	Todo.remove({}).then(() => {
-		Todo.insertMany(todos);
-	}).then(done);
+		return Todo.insertMany(todos);
+	}).then(() => done());
 };
 
 const populateUsers = (done) => {
-	User.remove({}).then(() => {
-		var userOne = new User(users[0]).save();
-		var userTwo = new User(users[1]).save();
+	User.remove({}).then(() => {done()});
+	// User.remove({}).then(() => {
+	// 	var userOne = new User(users[0]).save();
+	// 	var userTwo = new User(users[1]).save();
 
-		return Promise.all([userOne, userTwo]);
-	}).then(done);
+	// 	return Promise.all([userOne, userTwo])
+	// }).then(() => done());
 };
 
-module.exports = { todos, users , populateTodos, populateUsers };
+module.exports = { todos, populateTodos, users, populateUsers };
